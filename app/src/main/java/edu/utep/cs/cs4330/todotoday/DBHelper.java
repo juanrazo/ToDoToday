fab0ead22c691431fb93ab98b7708e75d88d7fce
juanrazo <jmrazo@miners.utep.edu>
2016-04-21 18:58:38 -0600

    ToDo with database

--- app/src/main/java/edu/utep/cs/cs4330/todotoday/DBHelper.java
+++ app/src/main/java/edu/utep/cs/cs4330/todotoday/DBHelper.java
@@ -7,47 +7,47 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
-/**
- * Created by juanrazo on 4/21/16.
- */
-public class DBHelper extends  SQLiteOpenHelper{
 
-    //TASK 1: DEFINE THE DATABASE TABLE
+class DBHelper extends SQLiteOpenHelper {
+
+    //TASK 1: DEFINE THE DATABASE AND TABLE
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "toDo_Today";
-    private static final String DATABASE_TABLE = "to_Do_Items";
+    private static final String DATABASE_TABLE = "toDo_Items";
+
 
     //TASK 2: DEFINE THE COLUMN NAMES FOR THE TABLE
-    private static final String KEY_TASK_ID = "id";
+    private static final String KEY_TASK_ID = "_id";
     private static final String KEY_DESCRIPTION = "description";
     private static final String KEY_IS_DONE = "is_done";
 
-    private int taskCount; //COUNTS THE NUMBER OF TASKS ON THE LIST
+    private int taskCount;
 
-    public DBHelper(Context context){
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    public DBHelper (Context context){
+        super (context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        String sqlStatement = "CREATE TABLE" + DATABASE_TABLE + "("
+    public void onCreate (SQLiteDatabase database){
+        String table = "CREATE TABLE " + DATABASE_TABLE + "("
                 + KEY_TASK_ID + " INTEGER PRIMARY KEY, "
                 + KEY_DESCRIPTION + " TEXT, "
                 + KEY_IS_DONE + " INTEGER" + ")";
-        db.execSQL(sqlStatement);
+        database.execSQL (table);
         taskCount = 0;
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
-        onCreate(db);
+    public void onUpgrade(SQLiteDatabase database,
+                          int oldVersion,
+                          int newVersion) {
+        database.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+        onCreate(database);
     }
 
-    //*********** DATABASE OPERATIONS: ADD, EDIT, DELETE
+    //********** DATABASE OPERATIONS:  ADD, EDIT, DELETE
 
-    //ADD A ToDo TASK TO THE DATABSE
+
     public void addToDoItem(ToDo_Item task) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
@@ -57,20 +57,19 @@ public class DBHelper extends  SQLiteOpenHelper{
         values.put(KEY_TASK_ID, taskCount);
 
         //ADD KEY-VALUE PAIR INFORMATION FOR THE TASK DESCRIPTION
-        values.put(KEY_DESCRIPTION, task.getDescription()); //task name
+        values.put(KEY_DESCRIPTION, task.getDescription()); // task name
 
         //ADD KEY-VALUE PAIR INFORMATION FOR IS_DONE
-        // 0- NOT DONE, 1 - IS DONE
+        //  0- NOT DONE, 1 - IS DONE
         values.put(KEY_IS_DONE, task.getIs_done());
 
         // INSERT THE ROW IN THE TABLE
         db.insert(DATABASE_TABLE, null, values);
 
-        //CLOSE THE DATABASE CONNECTION
+        // CLOSE THE DATABASE CONNECTION
         db.close();
     }
 
-    // EDIT A TODO TASK IN THE DATABASE
     public void editTaskItem(ToDo_Item task){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
@@ -85,7 +84,6 @@ public class DBHelper extends  SQLiteOpenHelper{
         db.close();
     }
 
-    // RETURN A SPECIFIC TODO TASK IN THE DATABASE
     public ToDo_Item getToDo_Task(int id) {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.query(
@@ -93,42 +91,43 @@ public class DBHelper extends  SQLiteOpenHelper{
                 new String[]{KEY_TASK_ID, KEY_DESCRIPTION, KEY_IS_DONE},
                 KEY_TASK_ID + "=?",
                 new String[]{String.valueOf(id)},
-                null, null, null, null);
+                null, null, null, null );
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
         ToDo_Item task = new ToDo_Item(
                 cursor.getInt(0),
                 cursor.getString(1),
                 cursor.getInt(2));
-
         db.close();
         return task;
     }
 
-    // DELETE A SPECIFIC TODO TASK FROM THE DATABASE
-    public  void deleteTaskItem (ToDo_Item task){
+    public void deleteTaskItem (ToDo_Item task){
         SQLiteDatabase database = this.getReadableDatabase();
 
         // DELETE THE TABLE ROW
         database.delete(DATABASE_TABLE, KEY_TASK_ID + " = ?",
-                new String[]{String.valueOf(task.getId())});
-
-        database.close();;
+                new String[]
+                        {String.valueOf(task.getId())});
+        database.close();
     }
 
     public int getTaskCount() {
-        return  taskCount;
+        return taskCount;
     }
 
-    // ADD A TODO TASK TO THE DATABASE
-    public ArrayList<ToDo_Item> getAllTaskItems(){
+    public ArrayList<ToDo_Item> getAllTaskItems() {
         ArrayList<ToDo_Item> taskList = new ArrayList<ToDo_Item>();
         String queryList = "SELECT * FROM " + DATABASE_TABLE;
 
         SQLiteDatabase database = this.getReadableDatabase();
         Cursor cursor = database.rawQuery(queryList, null);
 
-        // COLLECT EACH ROW IN THE TABLE
+        //COLLECT EACH ROW IN THE TABLE
         if (cursor.moveToFirst()){
-            do{
+            do {
                 ToDo_Item task = new ToDo_Item();
                 task.setId(cursor.getInt(0));
                 task.setDescription(cursor.getString(1));
@@ -136,10 +135,8 @@ public class DBHelper extends  SQLiteOpenHelper{
 
                 //ADD TO THE QUERY LIST
                 taskList.add(task);
-            }while(cursor.moveToNext());
+            } while (cursor.moveToNext());
         }
-        return  taskList;
+        return taskList;
     }
-
-
 }
--- app/src/main/java/edu/utep/cs/cs4330/todotoday/ToDo_Item.java
+++ app/src/main/java/edu/utep/cs/cs4330/todotoday/ToDo_Item.java
@@ -1,45 +1,41 @@
 package edu.utep.cs.cs4330.todotoday;
 
-/**
- * Created by juanrazo on 4/21/16.
- */
-public class ToDo_Item {
+class ToDo_Item {
 
-        //MEMBER ATRIBUTES
-        private int _id;
-        private String description;
-        private int is_done;
+    //MEMBER ATTRIBUTES
+    private int _id;
+    private String description;
+    private int is_done;
 
-        public ToDo_Item(){
-        }
+    public ToDo_Item() {
+    }
 
-        public ToDo_Item(int id, String desc, int done){
-            _id = id;
-            description = desc;
-            is_done = done;
-        }
+    public ToDo_Item(int id, String desc, int done) {
+        _id = id;
+        description = desc;
+        is_done = done;
+    }
 
-        public int getId(){
-            return _id;
-        }
+    public int getId() {
+        return _id;
+    }
+    public void setId(int id) {
+        _id = id;
+    }
 
-        public  void setId(int id){
-            _id = id;
-        }
+    public String getDescription () {
+        return description;
+    }
+    public void setDescription (String desc) {
+        description = desc;
+    }
 
-        public String getDescription(){
-            return description;
-        }
+    public int getIs_done() {
+        return is_done;
+    }
 
-        public void setDescription(String desc){
-            description = desc;
-        }
+    public void setIs_done(int done) {
+        is_done = done;
+    }
 
-        public int getIs_done(){
-            return is_done;
-        }
-
-        public void setIs_done(int done){
-            is_done = done;
-        }
 }
